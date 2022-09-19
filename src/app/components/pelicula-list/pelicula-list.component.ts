8dbfb70a7276e240542c5af82a7a6f428bc72627
Jimxx22 <jaumegil22@gmail.com>
2022-09-19 09:37:49 +0200

    first call

--- src/app/components/pelicula-list/pelicula-list.component.ts
+++ src/app/components/pelicula-list/pelicula-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-pelicula-list',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeliculaListComponent implements OnInit {
 
-  constructor() { }
+  peliculas:any;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get("https://api.themoviedb.org/3/movie/550?api_key=772f2146252e87b49a85442c0c01214a")
+    .subscribe(
+      result => {
+        this.peliculas = result;
+      },
+      error => {
+        console.log('error');
+      }
+    );
   }
 
 }
