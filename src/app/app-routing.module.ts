109875fbd29edd3cfdf52b40931f2605671eba06
Jimxx22 <jaumegil22@gmail.com>
2022-09-19 09:12:11 +0200

    components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from "./components/home/home.component";
+import { PeliculaDetailsComponent } from "./components/pelicula-details/pelicula-details.component";
+import { PeliculaListComponent } from "./components/pelicula-list/pelicula-list.component";
+import { SerieDetailsComponent } from "./components/serie-details/serie-details.component";
+import { SerieListComponent } from "./components/serie-list/serie-list.component";
+import { LoginComponent } from "./components/login/login.component";
+
+const routes: Routes = [
+  { path: 'home', component:HomeComponent },
+  { path: 'pelicula/:id', component:PeliculaDetailsComponent },
+  { path: 'peliculas', component:PeliculaListComponent },
+  { path: 'serie/:id', component:SerieDetailsComponent },
+  { path: 'series', component:SerieListComponent },
+  { path: 'login', component:LoginComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { PeliculaDetailsComponent } from './components/pelicula-details/pelicula-details.component';
+import { SerieDetailsComponent } from './components/serie-details/serie-details.component';
+import { PeliculaListComponent } from './components/pelicula-list/pelicula-list.component';
+import { SerieListComponent } from './components/serie-list/serie-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    PeliculaDetailsComponent,
+    SerieDetailsComponent,
+    PeliculaListComponent,
+    SerieListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
